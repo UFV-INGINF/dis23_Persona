1afc8450abee71f9605b44825e935ca419ad6520
Armando Berlanga <Jose.berlangam@udem.edu>
2022-10-05 20:21:52 +0200

    feat: agregue una linea

--- Persona/src/main/java/com/ufv/Main.java
+++ Persona/src/main/java/com/ufv/Main.java
@@ -3,5 +3,6 @@ package com.ufv;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hola soy Armando xdxd");
     }
 }

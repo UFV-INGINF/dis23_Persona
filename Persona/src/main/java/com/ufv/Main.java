80faa9416ecf38455f841fff9847bf128fddab0f
Nacho Serrano <ignaserrano@gmail.com>
2022-10-06 20:13:14 +0200

    We read file already

--- Persona/src/main/java/com/ufv/Main.java
+++ Persona/src/main/java/com/ufv/Main.java
@@ -23,55 +23,14 @@ public class Main {
         person.setDni("55555555A");
         person.setSexo(Sexo.sexo.v);
 
-        Persona person2 = new Persona();
+//        Persona person2 = new Persona();
         String Nombre;
 
-        System.out.print("Introduzca Nombre: ");
-        person2.setNombre(lectura.next());
 
-        System.out.print("Introduzca apellido: ");
-        person2.setApellido(lectura.next());
-
-        System.out.print("Enter Dni: ");
-        person2.setDni(lectura.next());
-
-//        person2.setSexo(Sexo.sexo.v);
-
-        System.out.print("Enter Address: ");
-        person2.setDireccion(lectura.next());
-
-        System.out.print("Introduzca email: ");
-        person2.setSexo(Sexo.sexo.valueOf(lectura.next()));
-        person2 = person;
-//
-//        Persona person2 = new Persona();
-//        String Nombre;
-//
-//        System.out.print("Introduzca Nombre: ");
-//        person2.setNombre(lectura.next());
-//
-//        System.out.print("Introduzca apellido: ");
-//        person2.setApellido(lectura.next());
-//
-//        System.out.print("Introduzca Dni: ");
-//        person2.setDni(lectura.next());
-//
-//        person2.setSexo(Sexo.sexo.v);
-//
-//        System.out.print("Introduzca Direccion: ");
-//        person2.setDireccion(lectura.next());
-//
-//        System.out.print("Introduzca Teléfono: ");
-//        person2.setTelefono(lectura.next());
-//
-//        System.out.println(person.toString());
-//        System.out.println(person2.toString());
-//        System.out.print("\n-----------------");
-//        System.out.print("SI NACHO LEE ESTO 1 PUNTO MAS PARA TODOS EN EL EXAMEN");
-//        System.out.print("-----------------\n");
-
-
-
+        System.out.println("Lectura fichero");
+        ArrayList<Persona> miLista = ReadFile.readFile();
+        System.out.println(miLista.toString());
+        System.out.println("-------------------------");
 
         personas.personaList.add(person);
         personas.personaList.add(person2);
--- Persona/src/main/java/com/ufv/ReadFile.java
+++ Persona/src/main/java/com/ufv/ReadFile.java
@@ -1,6 +1,49 @@
 package com.ufv;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class ReadFile {
+    static String line = "";
+    static String splitBy = ",";
+
+        //parsing a CSV file into BufferedReader class constructor
+        static BufferedReader br;
+
+
+        public static ArrayList<Persona> readFile() {
+
+            ArrayList<Persona> lista = new ArrayList<Persona>();
+            try {
+                br = new BufferedReader(new FileReader("agenda.csv"));
+                br.readLine();
+                while ((line = br.readLine()) != null)
+                //returns a Boolean value
+                {
+                    String[] persona = line.split(splitBy);
+                    Persona person = new Persona();
+                    person.setNombre(persona[0]);
+                    person.setApellido(persona[1]);
+                    person.setDni(persona[2]);
+//                    person.setSexo(Sexo.sexo.valueOf(persona[3]));
+
+                    lista.add(person);
+                    //use comma as separator
+                }
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            return lista;
+
+
+        }
+
 
 
 }

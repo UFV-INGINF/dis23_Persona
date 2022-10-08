68064e519c7f9d28d79b42bac360a4bcbbf39858
Inés Álvarez de Toledo <inesalvarezdetoledo@gmail.com>
2022-10-08 23:49:14 +0200

    Lectura de sexo del fichero arreglado

--- Persona/src/main/java/com/ufv/Main.java
+++ Persona/src/main/java/com/ufv/Main.java
@@ -1,8 +1,6 @@
 package com.ufv;
-import java.sql.Array;
+
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,10 +9,6 @@ public class Main {
         PersonaList personas = new PersonaList();
 
 
-//        System.out.println("Hello world!")
-//
-//        ;
-        Scanner lectura = new Scanner (System.in);
         Persona person = new Persona();
         Persona person2 = new Persona();
 
@@ -23,18 +17,15 @@ public class Main {
         person.setDni("55555555A");
         person.setSexo(Sexo.sexo.v);
 
-//        Persona person2 = new Persona();
-        String Nombre;
-
 
         System.out.println("Lectura fichero");
         ArrayList<Persona> miLista = ReadFile.readFile();
-        System.out.println(miLista.toString());
+        System.out.println(miLista);
         System.out.println("-------------------------");
 
         personas.personaList.add(person);
         personas.personaList.add(person2);
-        System.out.println(personas.personaList.toString());
+        System.out.println(personas.personaList);
         System.out.println(personas.personaList.size());
 
     }
